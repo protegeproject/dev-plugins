0dded4c4b38d65a3c5900863a0bc2ef71da97c6e
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-08-09 16:20:04 +0000

    Creating the advanced query plugin

---
+++ advancedQuery/src/edu/stanford/protege/query/AdvancedQueryPlugin.java
@@ -0,0 +1,18 @@
+package edu.stanford.smi.protege.query;
+
+import javax.swing.JLabel;
+
+import edu.stanford.smi.protege.resource.Icons;
+import edu.stanford.smi.protege.widget.AbstractTabWidget;
+
+public class AdvancedQueryPlugin extends AbstractTabWidget {
+
+  private static final long serialVersionUID = -5589620508506925170L;
+
+  public void initialize() {
+    setLabel("Advanced Query Tab");
+    setIcon(Icons.getInstancesIcon());
+    add(new JLabel("Query UI goes here"));
+  }
+
+}
