7d613230a99806accc633c5cd32b4013b490e1db
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-08-17 23:51:34 +0000

    Minor changes.

--- dev-plugins/trunk/advancedQuery/src/edu/stanford/smi/protege/query/InstallNarrowFrameStore.java
+++ dev-plugins/trunk/advancedQuery/src/edu/stanford/smi/protege/query/InstallNarrowFrameStore.java
@@ -7,11 +7,16 @@ import edu.stanford.smi.protege.exception.ProtegeException;
 import edu.stanford.smi.protege.model.Cls;
 import edu.stanford.smi.protege.model.DefaultKnowledgeBase;
 import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protege.model.Slot;
 import edu.stanford.smi.protege.model.framestore.NarrowFrameStore;
 import edu.stanford.smi.protege.model.framestore.SimpleFrameStore;
 import edu.stanford.smi.protege.util.ProtegeJob;
+import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.model.RDFNames;
 
 public class InstallNarrowFrameStore extends ProtegeJob {
+  public final static String RDF_LABEL = "rdf:label";
+  public final static String RDF_COMMENT = "rdf:comment";
   
   public InstallNarrowFrameStore(KnowledgeBase kb) {
     super(kb);
@@ -22,15 +27,26 @@ public class InstallNarrowFrameStore extends ProtegeJob {
     DefaultKnowledgeBase kb = (DefaultKnowledgeBase) getKnowledgeBase();
     SimpleFrameStore fs = (SimpleFrameStore) kb.getTerminalFrameStore();
     NarrowFrameStore nfs = fs.getHelper();
-    Object annotation = kb.getFrame("owl:AnnotationProperty");
-    Set<Cls> slotClasses = null;
-    if (annotation != null && annotation instanceof Cls) {
-      slotClasses = new HashSet<Cls>();
-      slotClasses.add((Cls) annotation);
-    }
-    QueryNarrowFrameStore qnfs = new QueryNarrowFrameStore(nfs, slotClasses);
+    
+    QueryNarrowFrameStore qnfs = new QueryNarrowFrameStore(nfs, getSearchableSlots());
     fs.setHelper(qnfs);
     return new Boolean(true);
+  }
+  
+  @SuppressWarnings("unchecked")
+  public Set<Slot> getSearchableSlots() {
+    DefaultKnowledgeBase kb = (DefaultKnowledgeBase) getKnowledgeBase();
+    Set<Slot> slots = new HashSet<Slot>();
+    if (kb instanceof OWLModel) {
+      OWLModel owl = (OWLModel) kb;
+      slots.addAll(owl.getOWLAnnotationProperties());
+      slots.add((Slot) owl.createRDFSNamedClass(RDF_LABEL));
+      slots.add((Slot) owl.createRDFSNamedClass(RDF_COMMENT));
+      return slots;
+    } else {
+      slots.addAll(kb.getSlots());
+    }
+    return slots;
   }
 
 }
