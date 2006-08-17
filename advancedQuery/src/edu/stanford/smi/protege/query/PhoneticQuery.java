053cdab5edce2a4bda40dc05cd2d0fe03de6543a
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-08-17 23:52:50 +0000

    Added getters and setters to the Phonetic Query class

--- advancedQuery/src/edu/stanford/smi/protege/query/PhoneticQuery.java
+++ advancedQuery/src/edu/stanford/smi/protege/query/PhoneticQuery.java
@@ -1,7 +1,28 @@
 package edu.stanford.smi.protege.query;
 
+import edu.stanford.smi.protege.model.Frame;
+import edu.stanford.smi.protege.model.Slot;
 import edu.stanford.smi.protege.model.query.Query;
 
 public class PhoneticQuery implements Query {
+  private Frame frame;
+  private Slot slot;
+  private String expr;
+  
+  public PhoneticQuery(Frame frame, Slot slot, String expr) {
+    this.frame = frame;
+    this.slot = slot;
+    this.expr = expr;
+  }
+  
+  public String getExpr() {
+    return expr;
+  }
+  public Frame getFrame() {
+    return frame;
+  }
+  public Slot getSlot() {
+    return slot;
+  }
 
 }
