9887ad2d2881658bae5dabf5007acd7172045cfb
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-08-20 22:46:15 +0000

    added some junits and they pass now

--- advancedQuery/junit/src/edu/stanford/smi/protege/query/SearchTest.java
+++ advancedQuery/junit/src/edu/stanford/smi/protege/query/SearchTest.java
@@ -2,15 +2,20 @@ package edu.stanford.smi.protege.query;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import junit.framework.TestCase;
 import edu.stanford.smi.protege.model.Frame;
 import edu.stanford.smi.protege.model.Project;
 import edu.stanford.smi.protege.model.Slot;
 import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protegex.owl.model.OWLClass;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
+import edu.stanford.smi.protegex.owl.model.RDFProperty;
 
 public class SearchTest extends TestCase {
+  private static transient Logger log  = Log.getLogger(SearchTest.class);
   
   /* ----------------------------------------------------------------------
    * Utility functions
@@ -20,7 +25,9 @@ public class SearchTest extends TestCase {
     List errors = new ArrayList();  
     Project project = new Project("advancedQuery/junit/projects/Pizza.pprj", errors);
     checkErrors(errors);
-    return (OWLModel) project.getKnowledgeBase();
+    OWLModel om = (OWLModel) project.getKnowledgeBase();
+    new InstallNarrowFrameStore(om).execute();
+    return om;
   }
   
   public static void checkErrors(List errors) {
@@ -48,9 +55,11 @@ public class SearchTest extends TestCase {
    */
 
 
-  public static void testBasicSearch() {   
+  public static void testBasicSearch() {
+    if (log.isLoggable(Level.FINE)) {
+      log.fine("basic search test");
+    }
     OWLModel om = getOWLModel();
-    new InstallNarrowFrameStore(om).execute();
     checkSearch(om, om.getRDFProperty("rdfs:comment"), "A class to demonstrate mistakes made", "IceCream", true);
     checkSearch(om, om.getRDFProperty("rdfs:comment"), "A Klass to demonstrate mistakes made", "IceCream", true);
     checkSearch(om, om.getRDFProperty("rdfs:comment"), "A Klass to demonstrate mistaques made", "IceCream", true);
@@ -60,11 +69,34 @@ public class SearchTest extends TestCase {
   }
   
   public static void testDelete() {
+    if (log.isLoggable(Level.FINE)) {
+      log.fine("delete phonetic search test");
+    }
     OWLModel om = getOWLModel();
-    new InstallNarrowFrameStore(om).execute();
     om.getOWLNamedClass("IceCream").delete();
-    checkSearch(om, om.getRDFProperty("rdfs:comment"), "A class to demonstrate mistakes made", "IceCream", true);
+    checkSearch(om, om.getRDFProperty("rdfs:comment"), "A class to demonstrate mistakes made", "IceCream", false);
   }
   
+  public static void testSetValues() {
+    if (log.isLoggable(Level.FINE)) {
+      log.fine("set values search test");
+    }
+    OWLModel om = getOWLModel();
+    OWLClass iceCream = om.getOWLNamedClass("IceCream");
+    RDFProperty comment = om.getRDFProperty("rdfs:comment");
+    iceCream.setPropertyValue(comment, "This is a real klass.  Don't make derogatory comments.");
+    checkSearch(om, comment, "class derogatory", "IceCream", true);
+  }
+  
+  public static void testAddValues() {
+    if (log.isLoggable(Level.FINE)) {
+      log.fine("add values search test");
+    }
+    OWLModel om = getOWLModel();
+    OWLClass iceCream = om.getOWLNamedClass("IceCream");
+    RDFProperty comment = om.getRDFProperty("rdfs:comment");
+    iceCream.addPropertyValue(comment, "But this class doesn't fit well with pizza.  And the comment wasn't derogatory.");
+    checkSearch(om, comment, "duznt derogatory", "IceCream", true);
+  }
 
 }
