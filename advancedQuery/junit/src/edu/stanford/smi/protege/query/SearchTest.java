ac2458233dc88c94dd35d5b6b3f3bc8d7710ed61
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-08-20 04:43:38 +0000

    Added two tests - the second one is not succeeding yet

--- advancedQuery/junit/src/edu/stanford/smi/protege/query/SearchTest.java
+++ advancedQuery/junit/src/edu/stanford/smi/protege/query/SearchTest.java
@@ -1,15 +1,27 @@
 package edu.stanford.smi.protege.query;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import junit.framework.TestCase;
 import edu.stanford.smi.protege.model.Frame;
 import edu.stanford.smi.protege.model.Project;
+import edu.stanford.smi.protege.model.Slot;
+import edu.stanford.smi.protege.util.Log;
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 
 public class SearchTest extends TestCase {
+  
+  /* ----------------------------------------------------------------------
+   * Utility functions
+   */
+  
+  public static OWLModel getOWLModel() {
+    List errors = new ArrayList();  
+    Project project = new Project("advancedQuery/junit/projects/Pizza.pprj", errors);
+    checkErrors(errors);
+    return (OWLModel) project.getKnowledgeBase();
+  }
   
   public static void checkErrors(List errors) {
     for (Object error : errors) {
@@ -20,21 +32,39 @@ public class SearchTest extends TestCase {
       }
     }
   }
-
-  /**
-   * @param args
-   */
-  public static void test01() {   
-    List errors = new ArrayList();  
-    Project project = new Project("advancedQuery/junit/projects/Pizza.pprj", errors);
-    checkErrors(errors);
-    OWLModel om = (OWLModel) project.getKnowledgeBase();
-    new InstallNarrowFrameStore(om).execute();
-    
-    PhoneticQuery pq = new PhoneticQuery(null, "A class to demonstrate mistakes made");
+  
+  public static void checkSearch(OWLModel om, Slot slot, String search, String frameName, boolean succeed) {
+    boolean found = false;
+    PhoneticQuery pq = new PhoneticQuery(slot, search);
     for (Frame frame : om.getHeadFrameStore().executeQuery(pq)) {
-      System.out.println("Found frame = " + frame);
+      assertTrue(frame.getName().equals(frameName));
+      found = true;
     }
-    System.out.println("Done");
+    assertTrue(succeed == found);
   }
+  
+  /* ---------------------------------------------------------------------
+   * Tests
+   */
+
+
+  public static void testBasicSearch() {   
+    OWLModel om = getOWLModel();
+    new InstallNarrowFrameStore(om).execute();
+    checkSearch(om, om.getRDFProperty("rdfs:comment"), "A class to demonstrate mistakes made", "IceCream", true);
+    checkSearch(om, om.getRDFProperty("rdfs:comment"), "A Klass to demonstrate mistakes made", "IceCream", true);
+    checkSearch(om, om.getRDFProperty("rdfs:comment"), "A Klass to demonstrate mistaques made", "IceCream", true);
+    checkSearch(om, om.getRDFProperty("rdfs:comment"), "A Klass to demonstrate missedakes made", "IceCream", true);
+    checkSearch(om, om.getRDFProperty("rdfs:comment"), "A Klass to demonstrate mistaches made", "IceCream", false);
+    Log.getLogger().info("Done");
+  }
+  
+  public static void testDelete() {
+    OWLModel om = getOWLModel();
+    new InstallNarrowFrameStore(om).execute();
+    om.getOWLNamedClass("IceCream").delete();
+    checkSearch(om, om.getRDFProperty("rdfs:comment"), "A class to demonstrate mistakes made", "IceCream", true);
+  }
+  
+
 }
