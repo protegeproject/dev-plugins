6f77190437645202be699baf3f2630d9d67c2a95
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-08-28 23:10:28 +0000

    Refactored so all the different types of queries are together in one place.

---
+++ dev-plugins/trunk/advancedQuery/src/edu/stanford/smi/protege/query/querytypes/PhoneticQuery.java
@@ -0,0 +1,24 @@
+package edu.stanford.smi.protege.query.querytypes;
+
+import edu.stanford.smi.protege.model.Slot;
+import edu.stanford.smi.protege.model.query.Query;
+
+public class PhoneticQuery implements Query {
+
+  private Slot slot;
+  private String expr;
+  
+  public PhoneticQuery(Slot slot, String expr) {
+    this.slot = slot;
+    this.expr = expr;
+  }
+  
+  public String getExpr() {
+    return expr;
+  }
+
+  public Slot getSlot() {
+    return slot;
+  }
+
+}
