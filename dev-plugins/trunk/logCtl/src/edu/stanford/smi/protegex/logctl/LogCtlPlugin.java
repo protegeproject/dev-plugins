a04414b073707498402a124a59d9c681f928ad34
vendetti <vendetti@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-11-16 19:55:43 +0000

    changed ui layout to use gridbadlayout

--- dev-plugins/trunk/logCtl/src/edu/stanford/smi/protegex/logctl/LogCtlPlugin.java
+++ dev-plugins/trunk/logCtl/src/edu/stanford/smi/protegex/logctl/LogCtlPlugin.java
@@ -1,73 +1,122 @@
 package edu.stanford.smi.protegex.logctl;
 
+// java - awt, swing
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.*;
+
+// java - util
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-
+// protege
+import edu.stanford.smi.protege.util.ComponentUtilities;
 import edu.stanford.smi.protege.widget.AbstractTabWidget;
 
 public class LogCtlPlugin extends AbstractTabWidget {
-    private static final long serialVersionUID = 1028676900020977636L;
+	private JButton getButton;
+	private JButton putButton;
+	private JComboBox levelDropDown;
+	private JLabel classLabel;
+	private JLabel levelLabel;
+	private JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+	private JPanel mainPanel = new JPanel(new GridBagLayout());
+	private JTextField locationField;
 
+    private static final long serialVersionUID = 1028676900020977636L;
     private final static Level[] levels = { Level.SEVERE, Level.WARNING, Level.INFO,
                                             Level.CONFIG, Level.FINE, Level.FINER,
                                             Level.FINEST };
-    
-    private JTextField locationField;
-    private JComboBox levelDropDown;
-    private JButton putButton;
-    private JButton getButton;
-    
 
     public void initialize() {
-        // initialize UI 
+		// Set the title for the tab widget.
         setLabel("Logger Control");
-        setLayout(new BorderLayout());
-        // setLayout(new GridLayout(3,2));
-        JPanel panel = new JPanel(new GridLayout(3,2));
-        panel.add(new JTextArea("Class/Password: "));
-        locationField = new JTextField(132);
-        panel.add(locationField);
-        
-        panel.add(new JTextArea("Level: "));
-        levelDropDown = new JComboBox();
-        for (Level level : levels) {
-            levelDropDown.addItem(level);
-        }
-        panel.add(levelDropDown);
-        putButton = new JButton("SetLevel");
-        putButton.addActionListener(new ActionListener() {
 
-            public void actionPerformed(ActionEvent event) {
-                String location = locationField.getText();
-                Level level = (Level) levelDropDown.getSelectedItem();
-                Logger log = Logger.getLogger(location);
-                log.setLevel(level);
-            }
-            
-        });
-        panel.add(putButton);
-        getButton = new JButton("getLevel");
-        getButton.addActionListener(new ActionListener() {
+		// Initialize & layout components.
+		GridBagConstraints c = new GridBagConstraints();
 
-            public void actionPerformed(ActionEvent arg0) {
-                String location = locationField.getText();
-                Logger log = Logger.getLogger(location);
-                levelDropDown.setSelectedItem(log.getLevel());
-            }
-            
-        });
-        panel.add(getButton);
-        add(panel, BorderLayout.CENTER);
-    }
+		// classLabel
+		classLabel = new JLabel("Class/Password:");
+		ComponentUtilities.setTitleLabelFont(classLabel);
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridwidth = 1;
+		c.gridheight = 1;
+		c.insets = new Insets(40, 0, 0, 10); // top padding (top, left, bottom, right for Insets objects)
+		c.fill = GridBagConstraints.NONE;
+		c.anchor = GridBagConstraints.LINE_END;
+		c.weightx = 0.0;
+		c.weighty = 0.0;
+		mainPanel.add(classLabel, c);
 
+        // levelLabel
+		levelLabel = new JLabel("Level:");
+		ComponentUtilities.setTitleLabelFont(levelLabel);
+		c.gridy = 1;
+		c.insets = new Insets(10, 0, 0, 10);
+		mainPanel.add(levelLabel, c);
+
+		// locationField
+		locationField = new JTextField(80);
+		c.gridx = 1;
+		c.gridy = 0;
+		c.insets = new Insets(40, 0, 0, 0); // top padding
+		c.anchor = GridBagConstraints.LINE_START;
+		mainPanel.add(locationField, c);
+
+		// levelDropDown
+		levelDropDown = new JComboBox();
+		initLevelsDropDown();
+		c.gridy = 1;
+		c.insets = new Insets(10, 0, 0, 0);
+		mainPanel.add(levelDropDown, c);
+
+		// buttons panel
+		initButtons();
+		buttonsPanel.add(getButton);
+		buttonsPanel.add(putButton);
+		c.gridy = 2;
+		mainPanel.add(buttonsPanel, c);
+
+		// Add main panel to tab plug-in.
+		setLayout(new BorderLayout());
+		add(mainPanel, BorderLayout.PAGE_START);
+	}
+
+	private void initLevelsDropDown() {
+		Dimension size = locationField.getPreferredSize();
+		size.width = 150;
+		levelDropDown.setPreferredSize(size);
+
+		for (Level level : levels) {
+			levelDropDown.addItem(level);
+		}
+	}
+
+	private void initButtons() {
+		getButton = new JButton("Get Level");
+		getButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String location = locationField.getText();
+			  	Logger log = Logger.getLogger(location);
+			  	levelDropDown.setSelectedItem(log.getLevel());
+		  	}
+	  	});
+
+		putButton = new JButton("Set Level");
+		putButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				String location = locationField.getText();
+				Level level = (Level) levelDropDown.getSelectedItem();
+				Logger log = Logger.getLogger(location);
+				log.setLevel(level);
+			}
+		});
+	}
 }
