40c50f0c0e55e0dd4eecc864cf0eb9e68bd83f6d
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-11-28 01:43:55 +0000

    A skeleton of the Protege load plugin.

---
+++ protegeLoad/src/edu/stanford/smi/protegex/load/LoadServiceImpl.java
@@ -0,0 +1,23 @@
+package edu.stanford.smi.protegex.load;
+
+import edu.stanford.smi.protege.model.Project;
+
+public class LoadServiceImpl implements LoadService {
+    
+    
+    
+    
+    public void addProject(Project p) {
+        
+    }
+    
+    public void removeProject(Project p) {
+        
+    }
+
+    public ProtegeLoadStats getLoad() {
+        ProtegeLoadStats stats = new ProtegeLoadStats();
+        return stats;
+    }
+
+}
