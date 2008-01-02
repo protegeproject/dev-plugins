899f347bb7a5cf3eae201439974ec169f1b4885a
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-01-02 18:54:59 +0000

    Moving the advanced query plugin to a new location and renaming it.

--- dev-plugins/trunk/advancedQuery/src/edu/stanford/smi/protege/query/querytypes/BoundableQuery.java
+++
@@ -1,9 +0,0 @@
-package edu.stanford.smi.protege.query.querytypes;
-
-public interface BoundableQuery extends VisitableQuery {
-	
-	void setMaxMatches(int maxMatches);
-	
-	BoundableQuery shallowClone();
-
-}
