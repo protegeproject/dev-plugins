caaf44e0465863c752b8b44dd894d90c9164ade7
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2009-10-08 17:45:19 +0000

    a little closer

--- autoId/src/uk/ac/man/cs/mig/coode/protege/id/PreferencesPanel.java
+++ autoId/src/uk/ac/man/cs/mig/coode/protege/id/PreferencesPanel.java
@@ -1,6 +1,5 @@
 package uk.ac.man.cs.mig.coode.protege.id;
 
-import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -9,27 +8,33 @@ import java.awt.event.ActionListener;
 import java.text.NumberFormat;
 
 import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JCheckBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 import edu.stanford.smi.protegex.owl.model.OWLModel;
 
 public class PreferencesPanel extends JPanel {
-    public JCheckBox           enabled;
-    public JCheckBox           uniqueId;
-    public JTextField          prefixField;
-    public JFormattedTextField digitCountField;
+    private JCheckBox           enabled;
+    private JRadioButton        uniqueId;
+    private JRadioButton        sequentialId;
+    private JTextField          prefixField;
+    private JFormattedTextField digitCountField;
     
+    private Preferences preferences;
     
     public PreferencesPanel(OWLModel owlModel) {
-        BoxLayout layout = new BoxLayout(this, BoxLayout.Y_AXIS);
+        preferences = new Preferences(owlModel);
         
+        BoxLayout layout = new BoxLayout(this, BoxLayout.Y_AXIS);
         setLayout(layout);
         
         enabled = new JCheckBox("Enabled?");
+        enabled.setSelected(preferences.isEnabled());
         enabled.setAlignmentX(Component.LEFT_ALIGNMENT);
         enabled.addActionListener(new ActionListener() {
             
@@ -39,7 +44,9 @@ public class PreferencesPanel extends JPanel {
         });
         add(enabled);
         
-        uniqueId = new JCheckBox("Use unique Ids?");
+        ButtonGroup uniqueIdGroup = new ButtonGroup();
+        uniqueId = new JRadioButton("Unique id");
+        uniqueIdGroup.add(uniqueId);
         uniqueId.addActionListener(new ActionListener() {
             
             public void actionPerformed(ActionEvent e) {
@@ -48,10 +55,23 @@ public class PreferencesPanel extends JPanel {
         });
         add(uniqueId);
         
+        sequentialId = new JRadioButton("Readable, sequential ids");
+        sequentialId.addActionListener(new ActionListener() {
+            
+            public void actionPerformed(ActionEvent e) {
+                updateEnabled();
+            }
+        });
+        uniqueIdGroup.add(sequentialId);
+        add(sequentialId);
+        
+        uniqueId.setSelected(preferences.isUniqueId());
+        
         JPanel prefixPanel = new JPanel();
         prefixPanel.add(new JLabel("Prefix: "));
         prefixField = new JTextField("ID");
         prefixField.setPreferredSize(new JTextField("A long id").getPreferredSize());
+        prefixField.setText(preferences.getPrefix());
         prefixPanel.add(prefixField);
         add(prefixPanel);
         
@@ -66,7 +86,7 @@ public class PreferencesPanel extends JPanel {
         double height = digitCountField.getPreferredSize().getHeight();
         double width  = new JLabel("999").getPreferredSize().getWidth();
         digitCountField.setPreferredSize(new Dimension((int) width, (int) height));
-        digitCountField.setValue(7);
+        digitCountField.setValue(preferences.getDigits());
         digitCountPanel.add(digitCountField);
         add(digitCountPanel);
     }
@@ -74,19 +94,26 @@ public class PreferencesPanel extends JPanel {
     private void updateEnabled() {
         if (!enabled.isSelected()) {
             uniqueId.setEnabled(false);
+            sequentialId.setEnabled(false);
             prefixField.setEnabled(false);
             digitCountField.setEnabled(false);
         }
-        else if (!uniqueId.isSelected()) {
+        else if (uniqueId.isSelected()) {
             uniqueId.setEnabled(true);
+            sequentialId.setEnabled(true);
             prefixField.setEnabled(false);
             digitCountField.setEnabled(false);
         }
         else {
             uniqueId.setEnabled(true);
+            sequentialId.setEnabled(true);
             prefixField.setEnabled(true);
             digitCountField.setEnabled(true);
         }
+    }
+    
+    public void save(OWLModel owlModel) {
+        preferences.save(owlModel);
     }
 
 }
