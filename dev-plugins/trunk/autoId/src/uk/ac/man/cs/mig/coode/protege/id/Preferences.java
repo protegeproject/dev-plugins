51b7f8b9f5cfc9ffa275f9882dbb781d7fe74ea7
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2009-10-08 00:52:32 +0000

    transfering to mac - added gui

--- dev-plugins/trunk/autoId/src/uk/ac/man/cs/mig/coode/protege/id/Preferences.java
+++ dev-plugins/trunk/autoId/src/uk/ac/man/cs/mig/coode/protege/id/Preferences.java
@@ -1,5 +1,42 @@
 package uk.ac.man.cs.mig.coode.protege.id;
 
+import edu.stanford.smi.protegex.owl.model.OWLModel;
+
 public class Preferences {
+    private boolean enabled;
+    private boolean uniqueId;
+    private String prefix;
+    private int digits;
+    
+    
+    public Preferences(OWLModel model) {
+        
+    }
+    
+    public boolean isEnabled() {
+        return enabled;
+    }
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+    public boolean isUniqueId() {
+        return uniqueId;
+    }
+    public void setUniqueId(boolean uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+    public String getPrefix() {
+        return prefix;
+    }
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+    public int getDigits() {
+        return digits;
+    }
+    public void setDigits(int digits) {
+        this.digits = digits;
+    }
+    
 
 }
